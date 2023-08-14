e178aa64497840b8b68fb21374709f33bbdd8ba2
unknown <upmanyu.agnihotri@resourcifi.com>
2023-08-14 16:47:51 +0530

    removed comments and final commit

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -48,7 +48,7 @@ export const Card = React.memo(function (props) {
                     src={cover}
                 />
             </div>
-            <div style={{height:"450px"}}class="newLaunchDetailDiv trendingDetailDiv">
+            <div style={{ height: "450px" }} class="newLaunchDetailDiv trendingDetailDiv">
                 <div class="newLunchiconsContainer">
                     <div class="newlaunchIconsLeft">
                         <div>
@@ -90,52 +90,6 @@ const Trending = () => {
             <div class="row">
                 <h3 class="d-flex justify-content-center p-2 mb-3 categoryHeading">Trending Now</h3>
             </div>
-            {/* <div class="row newLaunchCategoryRow">
-                {trending.map((item, index) => {
-                    return (
-                        <div class="col-4 mb-5" key={item?.id}>
-                            <div className='cardConatiner'>
-                                <div class="newLaunchImgCont">
-                                    <img class="newLaunchImg" alt="newLaunch" src={item?.path} />
-                                </div>
-                                <div class="newLaunchDetailDiv">
-                                    <div class="newLunchiconsContainer">
-                                        <div class="newlaunchIconsLeft">
-                                            <div>
-                                                <img class="newLaunchIconsImg" alt="eye" src="/eye.png" /> <span class="spanIconRight">200</span>
-                                            </div>
-                                            <div>
-                                                <img class="newLaunchIconsImg" alt="comment" src="/comment.png" /> <span class="spanIconRight">200</span>
-                                            </div>
-                                            <div>
-                                                <img class="newLaunchIconsImg" alt="share" src="/share.png" /> <span class="spanIconRight">200</span>
-                                            </div>
-                                            <div>
-                                                <img class="newLaunchIconsImg" alt="like" src="/like.png" /> <span class="spanIconRight">200</span>
-                                            </div>
-                                        </div>
-                                        <div class="newlaunchIconsRight">
-                                            <div class="rightSideIconsDiv">
-                                                <img class="newLaunchIconsImg" alt="collection" src="/collection.png" />
-                                            </div>
-                                            <div class="rightSideIconsDiv">
-                                                <img class="newLaunchIconsImg" alt="charm_share" src="/charm_share.png" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <p class="newLaunchDesc1">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and</p>
-                                    <p class="newLaunchDesc2">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not</p>
-                                    <div class="d-flex justify-content-between">
-                                        <img class="Peopleimg" alt="people" src="/people.png" />
-                                        <button class="discoverBtn">Discover Now</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                })}
-                <button class="moreBtn">More</button>
-            </div> */}
             <div style={{ width: "100%", position: "relative" }}>
                 <ResponsiveContainer
                     carouselRef={ref}
@@ -159,7 +113,7 @@ const Trending = () => {
                 />
                 <>
                     <Fab
-                        style={{ position: "absolute", top: "40%", left: 10, zIndex: 10 ,display:"none"}}
+                        style={{ position: "absolute", top: "40%", left: 10, zIndex: 10, display: "none" }}
                         size="small"
                         color="primary"
                         onClick={() => {
@@ -169,7 +123,7 @@ const Trending = () => {
                         <ArrowBackIcon />
                     </Fab>
                     <Fab
-                        style={{ position: "absolute", top: "40%", right: 10, zIndex: 10 ,display:"none"}}
+                        style={{ position: "absolute", top: "40%", right: 10, zIndex: 10, display: "none" }}
                         size="small"
                         color="primary"
                         onClick={() => {
